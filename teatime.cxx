63c790e17363e6286846b6d04957d1c880a30f22
Jordi Prats <jprats@systemadmin.es>
2016-06-21 17:55:10 +0200

    canvi titol finestra

--- teatime.cxx
+++ teatime.cxx
@@ -23,7 +23,7 @@ bool init()
 	}
 	else
 	{
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		gWindow = SDL_CreateWindow( "teatime", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( gWindow == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
